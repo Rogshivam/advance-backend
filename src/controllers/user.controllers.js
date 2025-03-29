97158a6515fc3ca9ea4f1057536c0f49ac5b4ed4
shivam <shivamk2000.com@gmail.com>
2025-03-29 19:35:11 +0530

    bug fixes and postman configure

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asynHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const registerUser = asyncHandler(async (req, res) => {
     //get user details from frontend
     // Validation -not empty
@@ -14,22 +16,25 @@ const registerUser = asyncHandler(async (req, res) => {
     //return res
 
     const { fullName, email, username, password} = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400, "Please fill all the fields")
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
     if(existedUser){
         throw new ApiError(409, "User already exists")
     }
-    const avatarLocalPath = req.files?.aavatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path ;
-
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path ;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath ){
         throw new ApiError(400, "Please upload avatar and cover image")
     }
@@ -37,9 +42,9 @@ const registerUser = asyncHandler(async (req, res) => {
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImages = await uploadOnCloudinary(coverImageLocalPath)
     if(!avatar ){
-        throw new ApiError(500, "Image upload failed")
+        throw new ApiError(400, "Image upload failed")
     };
-    User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImages: coverImages?.url || "",
@@ -48,13 +53,13 @@ const registerUser = asyncHandler(async (req, res) => {
         username: username.toLowerCase(),
         
     })
-    const createdUser = await User.findById(username._id).select("-password -refreshToken")
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
     if(!createdUser){
         throw new ApiError(500, "Something went wrong while registering the user")
     }
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully"),
-)
+    )
 })
 
 export { registerUser, }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,22 +3,24 @@ import fs from "fs";
 
    // Configuration
 cloudinary.config({ 
-    cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME',
-    api_key: 'process.env.CLOUDINARY_API_KEY',
-    api_secret: 'process.env.CLOUDINARY_API_SECRET'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
-const uploadOnCloudinary = async (loaclFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try{
-        if(!loaclFilePath) return null;
-        const response = await cloudinary.uploader.upload(loaclFilePath, {
-            resource_type: 'auto'
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         })
         //file has been uploaded successfully
-        console.log("File uploaded on cloudinary", response.url);
-        retrun.url;
+        // console.log("File uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+    
         return response;
     } catch (error) {
-        fs.unlinkSync(loaclFilePath); // delete the file from local storage
+        fs.unlinkSync(localFilePath); // delete the file from local storage
         return null;
     }
 
